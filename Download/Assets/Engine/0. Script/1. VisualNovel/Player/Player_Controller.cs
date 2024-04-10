a0f299091dcf62a106c7746eb411c8d217f3fd33
KimHana <gksk731@naver.com>
2024-04-10 22:50:16 +0900

    Feature : 미연시 플레이어 이동 수정

--- Download/Assets/Engine/0. Script/1. VisualNovel/Player/Player_Controller.cs
+++ Download/Assets/Engine/0. Script/1. VisualNovel/Player/Player_Controller.cs
@@ -6,6 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Transform m_mainCamera;
     [SerializeField] private float m_moveSpeed = 5.0f;
+    [SerializeField] private float m_lerpSpeed = 5.0f;
+
+    private Rigidbody m_rigidbody;
+
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+    }
 
     void FixedUpdate()
     {
@@ -18,22 +26,34 @@ public class PlayerController : MonoBehaviour
         transform.rotation = m_mainCamera.rotation;
 
         // 이동
-        float InputX = Input.GetAxis("Horizontal");
-        float InputZ = Input.GetAxis("Vertical");
-        if(InputX != 0.0f || InputZ != 0.0f)
-        {
-            Vector3 localDirection = new Vector3(InputX, 0, InputZ);
-            localDirection.Normalize();
-            Vector3 worldDirection = transform.TransformDirection(localDirection); // 이동 방향을 플레이어의 로컬 좌표계에서 월드 좌표계로 변환
-            worldDirection.Normalize();
+        Vector3 Velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.W))
+            Velocity += transform.forward * m_moveSpeed * Time.deltaTime;
+        else if (Input.GetKey(KeyCode.S))
+            Velocity += -transform.forward * m_moveSpeed * Time.deltaTime;
 
-            Vector3 rayOrigin = transform.position + new Vector3(0, 0.5f, 0);
-#if UNITY_EDITOR
-            Debug.DrawRay(rayOrigin, worldDirection * 0.4f, Color.red); // 레이 디버그 렌더
-#endif
-            if (!Physics.Raycast(rayOrigin, worldDirection, 0.4f, LayerMask.GetMask("Wall"))) // 벽이 없으면 해당 방향으로 이동.
-                transform.Translate(localDirection * m_moveSpeed * Time.deltaTime);           // Translate은 해당 방향에 콜라이더 여부를 검사하지 않고 이동함.
-        }
+        if (Input.GetKey(KeyCode.D))
+            Velocity += transform.right * m_moveSpeed * Time.deltaTime;
+        else if (Input.GetKey(KeyCode.A))
+            Velocity += -transform.right * m_moveSpeed * Time.deltaTime;
+        m_rigidbody.velocity = Vector3.Lerp(m_rigidbody.velocity, Velocity, Time.deltaTime * m_lerpSpeed);
+
+        // float InputX = Input.GetAxis("Horizontal");
+        // float InputZ = Input.GetAxis("Vertical");
+        // if(InputX != 0.0f || InputZ != 0.0f)
+        // {
+        //     Vector3 localDirection = new Vector3(InputX, 0, InputZ);
+        //     localDirection.Normalize();
+        //     Vector3 worldDirection = transform.TransformDirection(localDirection); // 이동 방향을 플레이어의 로컬 좌표계에서 월드 좌표계로 변환
+        //     worldDirection.Normalize();
+
+        //     Vector3 rayOrigin = transform.position + new Vector3(0, 0.5f, 0);
+        //#if UNITY_EDITOR
+        //     Debug.DrawRay(rayOrigin, worldDirection * 0.4f, Color.red); // 레이 디버그 렌더
+        //#endif
+        //     if (!Physics.Raycast(rayOrigin, worldDirection, 0.4f, LayerMask.GetMask("Wall"))) // 벽이 없으면 해당 방향으로 이동.
+        //          transform.Translate(localDirection * m_moveSpeed * Time.deltaTime);           // Translate은 해당 방향에 콜라이더 여부를 검사하지 않고 이동함.
+        //     }
     }
 
     private void OnCollisionEnter(Collision collision)
