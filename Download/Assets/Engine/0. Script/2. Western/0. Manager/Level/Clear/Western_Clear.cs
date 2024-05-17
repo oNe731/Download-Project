363da287e5902fe367b736c62af43319def5c871
KimHana <gksk731@naver.com>
2024-05-17 14:24:04 +0900

    Feature : 서부 네임스페이스 추가 적용

--- Download/Assets/Engine/0. Script/2. Western/0. Manager/Level/Clear/Western_Clear.cs
+++ Download/Assets/Engine/0. Script/2. Western/0. Manager/Level/Clear/Western_Clear.cs
@@ -2,32 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Western_Clear : Level
+namespace Western
 {
-    public Western_Clear(LevelController levelController) : base(levelController)
+    public class Western_Clear : Level
     {
-    }
+        public Western_Clear(LevelController levelController) : base(levelController)
+        {
+        }
 
-    public override void Enter_Level()
-    {
-        WesternManager.Instance.MainPanel.SetActive(true);
-        UIManager.Instance.Start_FadeIn(1f, Color.black);
-    }
+        public override void Enter_Level()
+        {
+            WesternManager.Instance.MainPanel.SetActive(true);
+            UIManager.Instance.Start_FadeIn(1f, Color.black);
+        }
 
-    public override void Play_Level()
-    {
-    }
+        public override void Play_Level()
+        {
+        }
 
-    public override void Update_Level()
-    {
-    }
+        public override void Update_Level()
+        {
+        }
 
-    public override void LateUpdate_Level()
-    {
-    }
+        public override void LateUpdate_Level()
+        {
+        }
 
-    public override void Exit_Level()
-    {
-        WesternManager.Instance.MainPanel.SetActive(false);
+        public override void Exit_Level()
+        {
+            WesternManager.Instance.MainPanel.SetActive(false);
+        }
     }
 }
+
--- Download/Assets/Engine/0. Script/2. Western/0. Manager/Level/Clear/Western_ClearLv2.cs
+++ Download/Assets/Engine/0. Script/2. Western/0. Manager/Level/Clear/Western_ClearLv2.cs
@@ -2,31 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Western_ClearLv2 : Western_Clear
+namespace Western
 {
-    public Western_ClearLv2(LevelController levelController) : base(levelController)
+    public class Western_ClearLv2 : Western_Clear
     {
-    }
+        public Western_ClearLv2(LevelController levelController) : base(levelController)
+        {
+        }
 
-    public override void Enter_Level()
-    {
-        base.Enter_Level();
-    }
+        public override void Enter_Level()
+        {
+            base.Enter_Level();
+        }
 
-    public override void Play_Level()
-    {
-    }
+        public override void Play_Level()
+        {
+        }
 
-    public override void Update_Level()
-    {
-    }
+        public override void Update_Level()
+        {
+        }
 
-    public override void LateUpdate_Level()
-    {
-    }
+        public override void LateUpdate_Level()
+        {
+        }
 
-    public override void Exit_Level()
-    {
-        base.Exit_Level();
+        public override void Exit_Level()
+        {
+            base.Exit_Level();
+        }
     }
 }
+
