c5ddae3b361caf08037eebf48577c906b2478898
GimOne <gksk731@naver.com>
2024-11-13 05:20:30 +0900

    Feature : 공포 단서 수정 및 임시 엔딩 작업

--- Download/Assets/Engine/0. Script/3. Horror/4. Object/Portal/Portal.cs
+++ Download/Assets/Engine/0. Script/3. Horror/4. Object/Portal/Portal.cs
@@ -17,6 +17,14 @@ public class Portal : MonoBehaviour
 
         // 페이드 아웃 -> 이동 -> 인
         GameManager.Ins.UI.EventUpdate = true;
-        GameManager.Ins.UI.Start_FadeOut(1f, Color.black, () => GameManager.Ins.Horror.LevelController.Change_Level((int)m_changelevel), 1f, false);
+        GameManager.Ins.UI.Start_FadeOut(1f, Color.black, () => Active_Ending(), 1f, false);
+        // GameManager.Ins.UI.Start_FadeOut(1f, Color.black, () => GameManager.Ins.Horror.LevelController.Change_Level((int)m_changelevel), 1f, false);
+    }
+
+    private void Active_Ending()
+    {
+        // 임시 엔딩
+        GameManager.Ins.UI.EventUpdate = true;
+        GameManager.Ins.UI.Start_FadeIn(0f, Color.black, () => GameObject.Find("Canvas").transform.GetChild(6).gameObject.SetActive(true));
     }
 }
